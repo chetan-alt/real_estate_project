81ad7ea8eb7b3470b0adb0421f64971bd54a5e53
Chetan <chetan081997@gmail.com>
2020-08-25 12:39:14 +0530

    Second commit

---
+++ listings/choices.py
@@ -0,0 +1,51 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4
+}
+
+price_choices = {
+    '15000000' : 'Rs.15,000,000',
+    '20000000' : 'Rs.20,000,000',
+    '23000000' : 'Rs.23,000,000',
+    '27000000' : 'Rs.27,000,000',
+    '30000000' : 'Rs.30,000,000',
+    '32000000' : 'Rs.32,000,000',
+    '35000000' : 'Rs.35,000,000',
+    '38000000' : 'Rs.38,000,000',
+    '40000000' : 'Rs.40,000,000',
+    '95000000' : 'Rs.95,000,000'
+}
+
+state_choices = {
+    'AP' : 'Andhra Pradesh',
+    'AR' : 'Arunachal Pradesh',
+    'AS' : 'Assam',
+    'BR' : 'Bihar',
+    'CG' : 'Chhatisgarh',
+    'GA' : 'Goa',
+    'GJ' : 'Gujarat',
+    'HR' : 'Haryana',
+    'HP' : 'Himachal Pradesh',
+    'JH' : 'Jharkhand',
+    'KA' : 'Karnataka',
+    'KL' : 'Kerala',
+    'MP' : 'Madhya Pradesh',
+    'MH' : 'Maharashtra',
+    'MN' : 'Manipur',
+    'ML' : 'Meghalaya',
+    'MZ' : 'Mizoram',
+    'NL' : 'Nagaland',
+    'OD' : 'Odisha',
+    'PB' : 'Punjab',
+    'RJ' : 'Rajasthan',
+    'SK' : 'Sikkim',
+    'TN' : 'Tamil Nadu',
+    'TL' : 'Telangana',
+    'TR' : 'Tripura',
+    'UP' : 'Uttar Pradesh',
+    'UK' : 'Uttarakhand',
+    'WB' : 'West Bengal'
+    }
+
